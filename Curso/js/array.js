fdbdb9353b0d2b37b1ae675babe72954ebab8bfe
arturxdking <victorxdking@gmail.com>
2024-03-15 17:01:57 -0300

    att

---
+++ Curso/js/array.js
@@ -0,0 +1,15 @@
+// var avgTemp = []
+
+// avgTemp[0] = 31.9
+// avgTemp[1] = 35.3
+// avgTemp[2] = 42
+// avgTemp[3] = 38
+// avgTemp[4] = 25.5
+
+// console.log(avgTemp[3]);
+
+let daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+for(let i = 0; i < daysOfWeek.length; i++) {
+    console.log(daysOfWeek[i]);
+}
