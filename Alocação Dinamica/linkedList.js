6ce190d846cdd1a60f2c8d36211aced7362e3d87
Victor Vieira <victorxdking@gmail.com>
2024-04-18 10:56:51 -0300

    aula 18/04

---
+++ Alocação Dinamica/linkedList.js
@@ -0,0 +1,23 @@
+function LinkedList() {
+
+    let Node = function(element) {
+        this.element = element;
+        this.next = null;
+    };
+
+    let length = 0;
+    let head = null;
+    this.append = function(element) {
+        let node = new Node(element), current;
+        if(head === null) {
+            head = node;
+        } else {
+            current = head;
+            while(current.next) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+        length++;
+    }
+};
